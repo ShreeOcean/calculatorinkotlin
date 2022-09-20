9d87fdadb655bd5b98304d41e73efa4b9b714c99
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-09-20 19:06:15 +0530

    implemented view binding

--- app/src/main/java/com/ocean/mycalculator/MainActivity.kt
+++ app/src/main/java/com/ocean/mycalculator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ocean.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ocean.mycalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // TextView used to display the input and output
+//    lateinit var txtInput: TextView
+    private lateinit var binding: ActivityMainBinding
+    // Represent whether the lastly pressed key is numeric or not
+    var lastNumeric: Boolean = false
+
+    // Represent that current state is in error or not
+    var stateError: Boolean = false
+
+    // If true, do not allow to add another DOT
+    var lastDot: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
